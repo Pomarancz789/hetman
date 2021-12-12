694c2c1bb3dd0ab3616e830911db74efd17cdd34
Pomarancz <kuba.wtorkiewicz@gmail.com>
2021-12-12 14:07:46 +0100

    Initial Commit

---
+++ hetman/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  hetman
+//
+//  Created by Kuba Wtorkiewicz on 12/12/2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
