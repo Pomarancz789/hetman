075c0aa1aae79c52a9204a3cf6552c35d8f2d9ba
Pomarancz <kuba.wtorkiewicz@gmail.com>
2021-12-12 17:11:06 +0100

    popełniam działające i skończone

--- hetman/main.cpp
+++ hetman/main.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main() {
-    int szachownica[8][8]={0};
+    int szachownica[8][8]={};
     int x=0;
     int y=0;
     
@@ -16,9 +16,70 @@ int main() {
     
     
     //zmniejszamy indexy o 1 ponieważ tablice zaczynają się od 0 a nie od 1
-    x=-1;
-    y=-1;
+    x=x-1;
+    y=y-1;
     //zmniejszamy indexy o 1 ponieważ tablice zaczynają się od 0 a nie od 1
+    
+    
+    //wszystkie ruchy w pionie i poziomie od pozycji hetmana na 1
+    for(int i=0;i<8;i++)
+    {
+        for(int j=0;j<8;j++)
+        {
+            if (j==x||i==y) {
+                szachownica[j][i]=1;
+            }
+        }
+    }
+    //wszystkie ruchy w pionie i poziomie od pozycji hetmana na 1
+    
+    
+    //ruchy w prawo w góre
+    for(int i=y; i>-1; i--)
+    {
+        for(int j=x;j<8;j++)
+        {
+            if(i+j==x+y)
+                szachownica[j][i]=1;
+        }
+    }
+    //ruchy w prawo w góre
+    
+    
+    //ruchy w prawo w dół
+    for(int i=y; i<8; i++)
+    {
+        for(int j=x;j<8;j++)
+        {
+            if(i-j==y-x)
+                szachownica[j][i]=1;
+        }
+    }
+    //ruchy w prawo w dół
+    
+    
+    //ruchy w lewo w dół
+    for(int i=y; i<8; i++)
+    {
+        for(int j=x;j>-1;j--)
+        {
+            if(j+i==y+x)
+                szachownica[j][i]=1;
+        }
+    }
+    //ruchy w lewo w dół
+    
+    
+    //ruchy w lewo w dół
+    for(int i=y; i>-1; i--)
+    {
+        for(int j=x;j>-1;j--)
+        {
+            if(i-j==y-x)
+                szachownica[j][i]=1;
+        }
+    }
+    //ruchy w lewo w dół
     
     
     //ustawiamy pozycję hetmana jako 2 w tablicy
@@ -26,14 +87,17 @@ int main() {
     //ustawiamy pozycję hetmana jako 2 w tablicy
     
     
-    for(int i=0;i<7;i++)
+    //wypisanie tablicy
+    for(int i=0;i<8;i++)
     {
-        for(int j=0;j<7;i++)
+        for(int j=0;j<8;j++)
         {
+            
             cout << szachownica[j][i] << "  ";
         }
         cout <<endl;
     }
+    //wypisanie tablicy
     
     return 0;
 }
